fab4ff8bb266fbe0f77682d58df7b8d4bbea257d
Zoran Stojakovic <stojakovic.zoran@gmail.com>
2014-07-04 22:29:13 +1000

    sample person

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,9 @@
+import simple.read.only.test.*
+
 class BootStrap {
 
     def init = { servletContext ->
+        new Person(name: "John").save(flush: true, failOnError: true)
     }
     def destroy = {
     }
